c6a98dae3a3d20bdecefc567bcd9ef272c81c038
SN-Noop <support@stamped.ninja>
2023-10-27 01:19:24 +0200

    fix: lib path

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve('src', 'lib/index.ts'),
-      name: 'bit-melody',
+      name: '@ninja-bit/melody',
       formats: ['es', 'umd'],
       fileName: (format) => `bit-melody.${format}.js`,
     },
